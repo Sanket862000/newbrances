89f37ae1539b536a271f66d3a76ff2ef2e604c41
sanket <sanket@sanket.example.com>
2024-08-08 23:33:21 -0400

    msg branch

---
+++ test.java
@@ -0,0 +1,45 @@
+$ git commit -m " "
+$ git status
+$ git log
+$ git push --all
+
+#chek branch
+$ git branch
+
+# * is head pointer or barch * show source branch present in barch 
+# create barch command
+$ git barnch dev
+$ git branch relase
+$ git branch
+
+# switch a braches command 
+$ git checkout dev
+$ git barnch
+$ ls -a
+
+#switcing a barches for one barche to second barnch
+$ git checkout realse
+$ git checkout master
+$ git branch
+# that show is * current barnch and another barch is traget barches or child braches 
+
+# create a new branch and switch direct this brach this cmd 
+$ git checkout -b relasse1.2
+
+
+# move is data one branch to another barnch 
+create file
+$relase 1.2
+$  git checkout master
+$ git merge relase1.2
+$ git log
+
+# 
+ $ git push --all
+
+
+
+
+
+
+
